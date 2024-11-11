1d561dd9a8df885c79974eba3f27139e4eff7530
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-11-11 23:03:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+
+CLIENT_ID = '436657411418-l9efj2619nabmtrm3bhfuilc6b08gm6g.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+
+
 # config.py
 API_ID = "15191874"
 API_HASH = "3037d39233c6fad9b80d83bb8a339a07"
-BOT_TOKEN = "6984238382:AAGwQWychtZSdRlnobbQLL9BIxdTidcynrU"
+TELEGRAM_BOT_TOKEN = "7481801715:AAEV22RePMaDqd2tyxH0clxtnqd5hDpRuTw"
