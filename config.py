47641b8dc4c4acca4f6e4d458abf8adc624ff75a
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-11-12 21:47:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
-CLIENT_ID = '436657411418-l9efj2619nabmtrm3bhfuilc6b08gm6g.apps.googleusercontent.com'
-CLIENT_SECRET = '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'
+#CLIENT_ID = '436657411418-l9efj2619nabmtrm3bhfuilc6b08gm6g.apps.googleusercontent.com'
+#CLIENT_SECRET = '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'
 
 
 # config.py
