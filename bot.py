cb93aafef9ba993b187b3cfc2ccc5e6c12a20fd0
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-11-19 06:39:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,8 +69,8 @@ async def add_watermark_async(video_path, output_path, text=DEFAULT_TEXT, positi
     out = cv2.VideoWriter(temp_video_path, fourcc, fps, (width, height))
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1
-    font_thickness = 1
+    font_scale = 0.5
+    font_thickness = 2
     border_thickness=1
     text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
 
